c66358095bd065b194b9a8f20f60294f5b072b4e
Will <willianson.silva@tpl.com.br>
2025-08-28 09:28:49 -0300

    feat: add jwt auth and encrypt secret

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,16 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
 import { SignUpDTO, SingInDTO } from './dtos/auth';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   async signup(data: SignUpDTO) {
     const UserAreadyExists = await this.prismaService.user.findUnique({
@@ -17,7 +23,13 @@ export class AuthService {
       throw new UnauthorizedException('User already exists');
     }
 
-    const user = await this.prismaService.user.create({ data });
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const user = await this.prismaService.user.create({
+      data: {
+        ...data,
+        password: hashedPassword,
+      },
+    });
 
     return {
       id: user.id,
@@ -26,6 +38,33 @@ export class AuthService {
     };
   }
   async signing(data: SingInDTO) {
-    return 'signing';
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Credentials invalid');
+    }
+
+    const doesPasswordMatch = await bcrypt.compare(
+      data.password,
+      user.password,
+    );
+
+    if (!doesPasswordMatch) {
+      throw new UnauthorizedException('Credentials invalid');
+    }
+
+    const accessToken = await this.jwtService.signAsync({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+
+    return {
+      accessToken,
+    };
   }
 }
