dbf2887fc3648f565f95aaf29ab2c3b39c339a0c
Will <willianson.silva@tpl.com.br>
2025-08-28 06:58:39 -0300

    feat: implement auth service and controller with signup and signing endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import type { SignUpDTO, SingInDTO } from './dtos/auth';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() body: SignUpDTO) {
+    await this.authService.signup(body);
+
+    return body;
+  }
+
+  @Post('signing')
+  async signing(@Body() body: SingInDTO) {
+    await this.authService.signing(body);
+
+    return body;
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { SignUpDTO, SingInDTO } from './dtos/auth';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async signup(data: SignUpDTO) {
+    return 'signup';
+  }
+  async signing(data: SingInDTO) {
+    return 'signing';
+  }
+}
